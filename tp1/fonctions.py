9fa7a0b9527e6c6bd6039c00dbf9e33f443e9427
Swazard <ybeaudouin789@gmail.com>
2024-09-16 16:16:52 +0200

    Changement fonction test

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,7 +1,9 @@
 def puissance(a, b):
-	if a == 0:
-		return 1
-	else:
-    	return(a**b)
+    if a == 0 and b > 0:
+        return 1
+    elif b == 0:
+        print("On ne peut pas diviser par 0")
+    else:
+        return(a**b)
     if not type(var) is int:
         raise TypeError("Only intergers are allowed")
